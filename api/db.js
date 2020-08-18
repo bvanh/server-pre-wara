01e24738743a0a8daad4692b9bd2787f85e918c6
bvanh <48560384+bvanh@users.noreply.github.com>
2020-08-18 08:55:49 +0700

    change port

--- api/db.js
+++ api/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || "vietanh",
   password: process.env.DB_PASS || "clappi1004##",
   database: process.env.DB_NAME || "pre_wara",
-  port: "8000",
+  port: "3307",
 });
 db.connect((err) => {
   if (err) {
